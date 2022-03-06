8544fde435e29db22ab3c48f7cfe60649963a3a3
Mateusz Bialowas <mateusz.bialowas2@gmail.com>
2022-03-06 21:41:07 +0100

    login servlet

---
+++ src/main/java/com/example/zad1/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.example.zad1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", value = "/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+
+
+        PrintWriter writer = response.getWriter();
+
+        String htmlRespone = "<html>";
+        htmlRespone += "<h2>Your username is: " + username + "<br/>";
+        htmlRespone += "Your password is: " + password + "</h2>";
+        htmlRespone += "</html>";
+        writer.println(htmlRespone);
+    }
+}
